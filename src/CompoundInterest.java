d469d6f74120ef84e8cf0aee7304e2750ba82514
Anton <volk.rea.by@gmail.com>
2022-12-03 14:15:41 +0100

    Commit 2

--- src/CompoundInterest.java
+++ src/CompoundInterest.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class CompoundInterest {
     public static void main(String[] args) {
 
-        int balance = 1000;
-        int year = 2;
-        double interestRate = 0.1;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Вас приветствует калькулятор доходности банковских вкладов.");
+        System.out.println("Введите сумму вклада:");
+        int balance = scanner.nextInt();
+        System.out.println("Срок хранения вклада в годах:");
+        int year = scanner.nextInt();
+        System.out.println("Процентная ставка в год:");
+        double interestRate = scanner.nextDouble() * 0.01;
 
         for (int i = 0; i < year; i++) {
             balance += balance * interestRate;
         }
+        System.out.println("Сумма по вкладу составит:");
         System.out.println(balance);
     }
 }
